24b496cf6a8bce8a9cc21c76bab5a1fab1973229
Ftnt <63697694+Ftnt@users.noreply.github.com>
2022-06-09 09:44:52 -0500

    modify port to 5000

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import app from "./src/libs/app";
 
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 5000);
 
 app.listen(app.get("port"), () => {
   console.log("Server is running on port ", app.get("port"));
