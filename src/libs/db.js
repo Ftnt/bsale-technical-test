a9ef0abcec374bd693d4cdbe0881aeee911326a9
Ftnt <63697694+Ftnt@users.noreply.github.com>
2022-06-09 00:54:02 -0500

    Add db configuration

---
+++ src/libs/db.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+require("dotenv").config();
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.RDS_HOST,
+  user: process.env.RDS_USER,
+  password: process.env.RDS_PASSWORD,
+  database: process.env.RDS_DB,
+});
+
+pool.getConnection(function (err, connection) {
+  if (err) throw err;
+  console.log("Successfully connected to the database.");
+  connection.release();
+});
+
+module.exports = pool;
