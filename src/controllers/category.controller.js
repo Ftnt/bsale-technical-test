833b647eb8227a30faa2eeffaa84118e01931d89
Ftnt <63697694+Ftnt@users.noreply.github.com>
2022-06-12 21:29:14 -0500

    add images for category

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,12 +1,24 @@
 import Category from "../usercases/category.case.js";
 
+const imgCategory = [
+  {id: "1", img:"https://i.ibb.co/6NN6N5Z/1.png"},
+  {id: "2", img:"https://i.ibb.co/JjLBfr1/2.png"},
+  {id: "3", img:"https://i.ibb.co/7np4TZs/3.png"},
+  {id: "4", img:"https://i.ibb.co/nDfSNgC/4.png"},
+  {id: "5", img:"https://i.ibb.co/2qjptr9/5.png"},
+  {id: "6", img:"https://i.ibb.co/HDmkwXp/6.png"},
+  {id: "7", img:"https://i.ibb.co/HKW9mcZ/7.png"},
+]
+
 export const getAllCategory = async (req, res) => {
   try {
     const allCategory = await Category.caseGetAllCategory();
+
+    let datas = allCategory?.map(dato => ({...dato , img : imgCategory.find(img => img.id === String(dato.id)).img}))
     return res.status(200).json({
       status: "success",
       message: `All Category`,
-      data: allCategory,
+      data: datas,
     });
   } catch (error) {
     return res.status(error.code).json({
